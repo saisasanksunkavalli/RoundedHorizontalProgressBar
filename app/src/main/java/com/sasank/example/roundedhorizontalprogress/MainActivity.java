e7fb37d9db3283e13607ed515807ea1ed40b1f69
saisasanksunkavalli <sasanksunkavalli@gmail.com>
2017-02-20 13:23:01 +0530

    Added more Examples

--- app/src/main/java/com/sasank/example/roundedhorizontalprogress/MainActivity.java
+++ app/src/main/java/com/sasank/example/roundedhorizontalprogress/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sasank.example.roundedhorizontalprogress;
 
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private RoundedHorizontalProgressBar mRoundedHorizontalProgressBar1;
     private RoundedHorizontalProgressBar mRoundedHorizontalProgressBar2;
 
+    private RoundedHorizontalProgressBar mRoundedHorizontalProgressBar4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         mRoundedHorizontalProgressBar1 = (RoundedHorizontalProgressBar) findViewById(R.id.progress_bar_1);
         mRoundedHorizontalProgressBar2 = (RoundedHorizontalProgressBar) findViewById(R.id.progress_bar_2);
+        mRoundedHorizontalProgressBar4 = (RoundedHorizontalProgressBar) findViewById(R.id.progress_bar_4);
 
         mRoundedHorizontalProgressBar1.animateProgress(2000, 0, 80);
 
-        mRoundedHorizontalProgressBar2.setProgressColors(getResources().getColor(R.color.blue_light), getResources().getColor(R.color.blue));
+        mRoundedHorizontalProgressBar2.setProgressColors(ResourcesCompat.getColor(getResources(), R.color.blue_light, null), ResourcesCompat.getColor(getResources(), R.color.blue, null));
         mRoundedHorizontalProgressBar2.animateProgress(1000, 0, 60);
+
+        mRoundedHorizontalProgressBar4.animateProgress(1000, 0 , 40);
     }
 }
